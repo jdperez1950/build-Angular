9ef990f58e98a4beea9aff1ac8032b2adb747bf9
Juan Daniel Perez Baez <44209776+JuanDaniel950@users.noreply.github.com>
2024-07-09 16:38:23 -0500

    new prod

--- chunk-4D7CZREC.js
+++
@@ -1,1 +0,0 @@
-import{b as o,c as r,d as e}from"./chunk-EBB6IUV5.js";import"./chunk-IDIJQKRB.js";import"./chunk-5C7ERPWK.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
