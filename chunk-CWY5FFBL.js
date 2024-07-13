31f6e07a95a50e40c992c9abaadc93584ba51c02
Juan Daniel Perez Baez <44209776+JuanDaniel950@users.noreply.github.com>
2024-07-13 12:08:20 -0500

    new prod

---
+++ chunk-CWY5FFBL.js
@@ -0,0 +1,1 @@
+import{e as o,f as r,g as e}from"./chunk-AIJAKKGR.js";import"./chunk-VLRYNVM6.js";import"./chunk-SKOGUYFK.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
