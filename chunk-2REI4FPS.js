8f38baba90cecffd9008870ba9b1b44c06221578
Juan Daniel Perez Baez <44209776+JuanDaniel950@users.noreply.github.com>
2024-07-15 20:30:36 -0500

    git add .

--- chunk-2REI4FPS.js
+++
@@ -1,1 +0,0 @@
-import{e as o,f as r,g as e}from"./chunk-IZCPZQPB.js";import"./chunk-TVMO4XU5.js";import"./chunk-LJQ4BKAM.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
