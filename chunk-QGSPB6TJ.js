536e065b8405bd2071f9dbd1f332694b9a585e2c
Juan Daniel Perez Baez <44209776+JuanDaniel950@users.noreply.github.com>
2024-07-18 17:04:41 -0500

    add

--- chunk-QGSPB6TJ.js
+++
@@ -1,1 +0,0 @@
-import{e as o,f as r,g as e}from"./chunk-6GFFKMXZ.js";import"./chunk-WFSCHGYV.js";import"./chunk-VWAIX4TX.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
