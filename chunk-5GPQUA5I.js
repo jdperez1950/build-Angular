613496df627b0801533daff192aa9e9b8d7c1c65
Juan Daniel Perez Baez <44209776+JuanDaniel950@users.noreply.github.com>
2024-08-01 20:41:05 -0500

    ajutes 01/08

--- chunk-5GPQUA5I.js
+++
@@ -1,1 +0,0 @@
-import{h as o,i as r,j as e}from"./chunk-OVNM43TH.js";import"./chunk-77WZTTAJ.js";import"./chunk-Q4M4WEAW.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
---
+++ chunk-S3VJ7EFQ.js
@@ -0,0 +1,1 @@
+import{h as o,i as r,j as e}from"./chunk-ZAHKGYMH.js";import"./chunk-5LRYQ4UL.js";import"./chunk-Q4M4WEAW.js";var i=[{path:"",component:r},{path:"registrar-beneficiario",component:e},{path:"registrar-proveedor",component:o}];export{i as AUHT_ROUTES};
